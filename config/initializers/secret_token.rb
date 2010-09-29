1403d13fc4b1f1a41964f9a09cd00d1f8cc91ad7
Benny <benny@zooworkshop.com>
2010-09-29 09:51:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S3certified::Application.config.secret_token = '86fac50fae463d1cd5cd16d191e7fef7916a1e7f62c1d2ed5c4df77c0ceef0e0dce2f1840417c8e5aa9d8bf7e60ef1d50e47f5ff38106e64de2c80be8d2c8035'
