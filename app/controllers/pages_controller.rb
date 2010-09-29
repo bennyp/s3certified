5501a3044dee0b7a2ab4d98d7a65726757b070b9
Benny <benny@zooworkshop.com>
2010-09-29 12:11:17 -0700

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def seller
+    @title = "Seller"
   end
 
   def buyer
+    @title = "Buyer"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def about
+    @title = "About"
   end
 
 end
