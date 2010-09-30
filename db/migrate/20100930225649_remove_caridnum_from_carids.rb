901be399a0878c0f38720fda1398ae65827414fb
Benny <benny@zooworkshop.com>
2010-09-30 16:28:33 -0700

    Removed migration

--- db/migrate/20100930225649_remove_caridnum_from_carids.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCaridnumFromCarids < ActiveRecord::Migration
-  def self.up
-    remove_column :carids, :caridnum
-  end
-
-  def self.down
-    add_column :carids, :caridnum, :string
-  end
-end
